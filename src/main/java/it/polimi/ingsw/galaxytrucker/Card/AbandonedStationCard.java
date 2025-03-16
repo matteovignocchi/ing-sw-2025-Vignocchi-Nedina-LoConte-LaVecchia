5b32101d05fdd85d9a280babf7f8f42f8da6d9d9
24GIOCONDO24 <kekko.lc03@gmail.com>
2025-03-16 01:41:02 +0100

    Implementazione carte avventura

---
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Card/AbandonedStationCard.java
@@ -0,0 +1,50 @@
+package it.polimi.ingsw.galaxytrucker.Card;
+import it.polimi.ingsw.galaxytrucker.FlightCardBoard;
+import it.polimi.ingsw.galaxytrucker.Player;
+import it.polimi.ingsw.galaxytrucker.Token.Good;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class describes the behavior of the "Abandoned Station" adventure card.
+ * @author Francesco Lo Conte && Gabriele La Vecchia
+ */
+
+public class AbandonedStationCard implements Card {
+    private final int num_crewmates;
+    private final int days;
+    private final List<Good> station_goods;
+
+    public AbandonedStationCard(int num_crewmates, int days, List<Good> station_goods){
+        this.num_crewmates = num_crewmates;
+        this.days = days;
+        this.station_goods = new ArrayList<>(station_goods);
+    }
+
+    /**
+     * Activate the card's effect: It goes through the list of players in order starting from the leader and checks,
+     * one at a time, if the number of crewmates is greater than or equal to "num_crewmates" and, if so, then asks
+     * the player if he wants to lose "days" days of flight and load the list of goods "station_goods" on his ship.
+     * If the player accepts, the loop is interrupted.
+     * @param players Players list sorted by position, from the leader onwards
+     * @param f FlightCardBoard
+     * @throws IllegalArgumentException exception thrown if (see conditions below)
+     */
+
+    @Override
+    public void activate(List<Player> players, FlightCardBoard f) throws IllegalArgumentException{
+        if(players == null) throw new IllegalArgumentException("Null players list");
+        else if(players.isEmpty()) throw new IllegalArgumentException("Empty players list");
+        else if(f == null) throw new IllegalArgumentException("Null flight card board");
+
+        for(Player p: players) {
+            if(p.getCrewmates()>=num_crewmates){
+                if(p.askAbandonedStation(days, station_goods)){
+                    f.moveRocket(-days, p, players);
+                    p.addGoods(station_goods);
+                }
+                break;
+            }
+        }
+    }
+}
