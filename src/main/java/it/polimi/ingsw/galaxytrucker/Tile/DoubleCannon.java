bea3efda561bd26fcb11d57660e86b07fed1c1db
Oleg Nedina <oleg.nedina@polimi.mail.it>
2025-03-16 13:15:00 +0100

    new double cannon

--- src/main/java/it/polimi/ingsw/galaxytrucker/Tile/DoubleCannon.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Tile/DoubleCannon.java
@@ -7,7 +7,6 @@ package it.polimi.ingsw.galaxytrucker.Tile;
  */
 public class DoubleCannon extends Tile {
     private double power;
-    private boolean isOn;
 
     public DoubleCannon(int a,int b,int c,int d) {
         corners[0]=a;
@@ -15,7 +14,6 @@ public class DoubleCannon extends Tile {
         corners[2]=c;
         corners[3]=d;
         power = 0;
-        isOn = false;
     }
     /**
      * the method besides changing the direction it changes the firepower
@@ -28,7 +26,6 @@ public class DoubleCannon extends Tile {
         corners[2] = corners[1];
         corners[1] = corners[0];
         corners[0] = tmp;
-        if( corners[0] != 4) power = 0.5;
     }
     /**
      * the method besides changing the direction it changes the firepower
@@ -41,33 +38,19 @@ public class DoubleCannon extends Tile {
         corners[1] = corners[2];
         corners[2] = corners[3];
         corners[3] = tmp;
-        if( corners[0] != 4) power = 0.5;
     }
-
-    /**
-     * changes the status of the cannon
-     */
-    public void Start(){
-        isOn = true;;
-    }
-    public void TurnOf(){
-        isOn = false;
-    }
-    /**
-     * @return the actual firepower
-     */
     public double getPower(boolean active) {
 
         if(active){
-            return power+2;
+            if(this.controlCorners(0) != 5){
+            return power+1;
+            }else {return power+2;}
         }
         else {
             return power;
         }
     }
-    public boolean getStatus(){
-        return isOn;
-    }
+
 
     }
 
