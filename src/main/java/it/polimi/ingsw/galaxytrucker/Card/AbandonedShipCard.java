566aa909a90ec5ef4a2fb24f6e9cb8d2c5c8e2f5
24GIOCONDO24 <kekko.lc03@gmail.com>
2025-04-14 19:24:29 +0200

    Update CardEffectVisitor

--- src/main/java/it/polimi/ingsw/galaxytrucker/Card/AbandonedShipCard.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Card/AbandonedShipCard.java
@@ -1,8 +1,4 @@
 package it.polimi.ingsw.galaxytrucker.Card;
-import it.polimi.ingsw.galaxytrucker.FlightCardBoard;
-import it.polimi.ingsw.galaxytrucker.Player;
-import java.util.List;
-
 /**
  * This class describes the behavior of the "Abandoned Ship" adventure card.
  * @author Francesco Lo Conte && Gabriele La Vecchia
@@ -20,29 +16,9 @@ public class AbandonedShipCard implements Card{
         this.num_crewmates = num_crewmates;
     }
 
-    /**
-     * Activate the card's effect: It scrolls down the list of players in order starting from the leader
-     * and asks the player if he wants to activate the card effect. If he accepts, the rocket is moved back "days"
-     * spaces on the FlightCardBoard, "credits" cosmic credits are added and "num_crewmates" crew tokens are removed
-     * and the loop is exited since only one player can use the card effect
-     * @param players Players list sorted by position, from the leader onwards
-     * @param f FlightCardBoard
-     */
-
     @Override
-    public void activate(List<Player> players, FlightCardBoard f){
-        if(players == null) throw new NullPointerException("Null players list");
-        else if(players.isEmpty()) throw new IllegalArgumentException("Empty players list");
-        else if(f == null) throw new NullPointerException("Null flight card board");
-
-        for(Player p : players){
-            if(p.askPlayerDecision()) {
-                f.moveRocket(-days, p, players);
-                p.addCredits(credits);
-                p.removeCrewmates(num_crewmates);
-                break;
-            }
-        }
+    public void accept(CardVisitor visitor){
+        visitor.visit(this);
     }
 
     public int getDays(){ return days; }
--- src/main/java/it/polimi/ingsw/galaxytrucker/Card/CardVisitor.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Card/CardVisitor.java
@@ -4,8 +4,14 @@ public interface CardVisitor {
     void visit(OpenSpaceCard card);
     void visit(StardustCard card);
     void visit(SmugglersCard card);
-    void visit (SlaversCard card);
+    void visit(SlaversCard card);
     void visit(SecondWarzoneCard card);
     void visit(FirstWarzoneCard card);
+    void visit(AbandonedShipCard card);
+    void visit(AbandonedStationCard card);
+    void visit(MeteoritesRainCard card);
+    void visit(PiratesCard card);
+    void visit(PlanetsCard card);
+    void visit(PlaugeCard card);
     // inserire gli altri visit
 }
