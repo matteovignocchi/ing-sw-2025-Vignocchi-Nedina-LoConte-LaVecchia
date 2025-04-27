fcdf0be94dc9bd4e5edccf01c390b222dc47dbad
gabrielelv <gabrielelavecchia.cpt@gmail.com>
2025-04-27 18:08:50 +0200

    boh

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module it.polimi.ingsw.galaxytrucker {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires kotlin.stdlib;
+    requires jdk.xml.dom;
+    requires java.smartcardio;
+    requires com.fasterxml.jackson.databind;
+    requires java.rmi;
+    requires annotations;
+    requires java.naming;
+
+    opens it.polimi.ingsw.galaxytrucker to javafx.fxml;
+    exports it.polimi.ingsw.galaxytrucker;
+
+}
