badf561d3a84de664212fa3b1697ac4279fa3d0e
Matteo Vignocchi <MATTEO.VIGNOCCHI03@GMAIL.COM>
2025-05-13 14:52:37 +0200

    fix

--- src/main/java/it/polimi/ingsw/galaxytrucker/View/GUI/SceneEnum.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/View/GUI/SceneEnum.java
@@ -3,7 +3,7 @@ package it.polimi.ingsw.galaxytrucker.View.GUI;
 public enum SceneEnum {
     MAIN_MENU("/polimi/ingsw/fxml/MainMenu.fxml"),
     CREATE_GAME_MENU("/polimi/ingsw/fxml/CreateGameMenu.fxml"),
-    BUILDING_PHASE("/polimi/ingsw/fxml/BuildingPhase.fxml"),
+    BUILDING_PHASE("/polimi/ingsw/fxml/BuildingPhase.fxml");
     private final String value;
     SceneEnum(final String value) {
         this.value = value;
