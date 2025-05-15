df2c5bd149dd4e4c09db51b6a9a41f82e7666bdb
gabrielelv <gabrielelavecchia.cpt@gmail.com>
2025-05-15 15:56:23 +0200

    .

--- src/main/java/it/polimi/ingsw/galaxytrucker/Model/Card/PlaugeCard.java
+++ src/main/java/it/polimi/ingsw/galaxytrucker/Model/Card/PlaugeCard.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.galaxytrucker.Model.Card;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import it.polimi.ingsw.galaxytrucker.BusinessLogicException;
 
 /**
  * This class handles the PlaugeCard, which is deserialized via Jackson.
@@ -21,7 +22,7 @@ public class PlaugeCard implements Card {
     }
 
     @Override
-    public void accept(CardVisitor visitor) throws CardEffectException {
+    public void accept(CardVisitor visitor) throws BusinessLogicException {
         visitor.visit(this);
     }
 
