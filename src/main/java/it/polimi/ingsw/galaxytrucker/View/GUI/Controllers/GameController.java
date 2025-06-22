2dfa66d8b6f0af90b9c2abd0b82dd7c431b80606
Matteo Vignocchi <MATTEO.VIGNOCCHI03@GMAIL.COM>
2025-06-22 17:06:36 +0200

    aggiunte

---
+++ src/main/java/it/polimi/ingsw/galaxytrucker/View/GUI/Controllers/GameController.java
@@ -0,0 +1,137 @@
+package it.polimi.ingsw.galaxytrucker.View.GUI.Controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+public class GameController extends GUIController{
+    @FXML
+    private Pane demo1;
+    @FXML
+    private Pane demo2;
+    @FXML
+    private Pane demo3;
+    @FXML
+    private Pane demo4;
+    @FXML
+    private Pane demo5;
+    @FXML
+    private Pane demo6;
+    @FXML
+    private Pane demo7;
+    @FXML
+    private Pane demo8;
+    @FXML
+    private Pane demo9;
+    @FXML
+    private Pane demo10;
+    @FXML
+    private Pane demo11;
+    @FXML
+    private Pane demo12;
+    @FXML
+    private Pane demo13;
+    @FXML
+    private Pane demo14;
+    @FXML
+    private Pane demo15;
+    @FXML
+    private Pane demo16;
+    @FXML
+    private Pane demo17;
+    @FXML
+    private Pane demo18;
+    @FXML
+    private Pane path1;
+    @FXML
+    private Pane path2;
+    @FXML
+    private Pane path3;
+    @FXML
+    private Pane path4;
+    @FXML
+    private Pane path5;
+    @FXML
+    private Pane path6;
+    @FXML
+    private Pane path7;
+    @FXML
+    private Pane path8;
+    @FXML
+    private Pane path9;
+    @FXML
+    private Pane path10;
+    @FXML
+    private Pane path11;
+    @FXML
+    private Pane path12;
+    @FXML
+    private Pane path13;
+    @FXML
+    private Pane path14;
+    @FXML
+    private Pane path15;
+    @FXML
+    private Pane path16;
+    @FXML
+    private Pane path17;
+    @FXML
+    private Pane path18;
+    @FXML
+    private Pane path19;
+    @FXML
+    private Pane path20;
+    @FXML
+    private Pane path21;
+    @FXML
+    private Pane path22;
+    @FXML
+    private Pane path23;
+    @FXML
+    private Pane path24;
+    @FXML
+    private ImageView ship1;
+    @FXML
+    private ImageView ship2;
+    @FXML
+    private GridPane imageShip;
+    @FXML
+    private Button logout;
+    @FXML
+    private HBox messageBox;
+    @FXML
+    private TextFlow messageTextFlow;
+    @FXML
+    private Text messageText;
+    @FXML
+    private Label nicknametext;
+    @FXML
+    private Label position;
+    @FXML
+    private Label credits;
+    @FXML
+    private Label enginepower;
+    @FXML
+    private Label firepower;
+    @FXML
+    private Label purplealien;
+    @FXML
+    private Label brownalien;
+    @FXML
+    private Label numofhumans;
+    @FXML
+    private Label energycell;
+
+
+
+
+
+
+    
+}
